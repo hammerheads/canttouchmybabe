adbfc453f0889671aaa931358d07c72b00ad9ff1
Joao Ribeiro <jppribeiro@gmail.com>
2019-08-08 22:13:28 +0100

    babe form sends to API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const API = 'http://localhost:3000';
+
 $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
@@ -18,3 +20,34 @@ $( document ).ready(function () {
         }
     })
 })
+
+
+function submitBabe() {
+
+    $("#cid").val("1");
+
+    console.log($("#cid"));
+    
+
+    $.ajax({
+        url: API + '/customer/' + $("#cid").val() + "/babe/",
+        type: 'POST',
+        data: JSON.stringify({
+            firstName: $("#firstName").val(),
+            lastName: $("#lastName").val(),
+            email: $("#email").val(),
+            phone: $("#phone").val(),
+            address: $("#address").val(),
+            locations: [$("#locations1").val(), $("#locations2").val(), $("#locations3").val()]
+        }),
+        async: true,
+        contentType: "application/json",
+        success: function() {
+            console.log("success");
+        },
+        error: function(status, error) {
+            console.log('Something went wrong: ', status, error);
+        }
+    })
+
+}
